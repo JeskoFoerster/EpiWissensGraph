b32d8f32dd712aa173c8dd8e4465c55dd70fe327
Tom <tom.dominik.demuth@gmail.com>
2023-11-27 11:46:31 +0100

    Added the "Aldi Nord" Node as well as an image and a prototype content text.

--- Subgraphs/CompanyNodes/AldiNord_Data.py
+++ Subgraphs/CompanyNodes/AldiNord_Data.py
@@ -1,7 +1,14 @@
-# id:12
+# id:39
 
-TITEL = "AldiNord"
+TITEL = "Aldi Nord"
 
-CONTENT = ("Coseon ist eine Sotware Firma")
+CONTENT = ("Aldi Nord ist eine der führenden Einzelhandelsketten in Deutschland, die sich durch ihre herausragende Präsenz und einen effizienten Betrieb auszeichnet. Mit stolzen 2,200 Filialen im gesamten Bundesgebiet bietet Aldi Nord seinen Kunden eine breite Palette von hochwertigen Produkten zu erschwinglichen Preisen. "
+           "Diese beeindruckende Anzahl von Filialen spiegelt die starke Präsenz und den flächendeckenden Service wider, den Aldi Nord seinen Kunden bietet."
+           "Die Erfolgsgeschichte von Aldi Nord wird nicht nur durch die zahlreichen Filialen, sondern auch durch das engagierte Team von 40,000 Mitarbeitern in Deutschland geprägt. "
+           "Diese Mitarbeiter tragen maßgeblich dazu bei, dass Aldi Nord seinen Kunden qualitativ hochwertige Produkte zu wettbewerbsfähigen Preisen anbieten kann. Ihre Professionalität und ihr Einsatz sind Schlüsselfaktoren für den Erfolg und die Beliebtheit von Aldi Nord."
+           "Eine weitere Stärke von Aldi Nord ist die moderne Ausrichtung auf die Bedürfnisse der Kunden. Die Website von Aldi Nord, erreichbar unter https://www.aldi-nord.de, ist ein wichtiger Bestandteil dieser Strategie. "
+           "Hier können Kunden nicht nur das aktuelle Sortiment und Sonderangebote einsehen, sondern auch nützliche Informationen zu Filialstandorten, Öffnungszeiten und vielem mehr erhalten. Die Website bietet eine benutzerfreundliche Oberfläche, die den Online-Einkauf und die Informationsbeschaffung für die Kunden erleichtert."
+           "Aldi Nord hat sich im deutschen Einzelhandel fest etabliert und setzt weiterhin auf Qualität, Kundennähe und wirtschaftliche Effizienz. "
+           "Die Kombination aus einer großen Anzahl von Filialen, engagierten Mitarbeitern und einer modernen Online-Präsenz macht Aldi Nord zu einer vertrauenswürdigen Anlaufstelle für Millionen von Kunden in Deutschland.")
 
-IMAGE_NAME = "coseon.png"
+IMAGE_NAME = "AldiNord_logo.png"
--- Subgraphs/ConsumerGoodsGraph.py
+++ Subgraphs/ConsumerGoodsGraph.py
@@ -12,22 +12,23 @@ class ConsumerGoodsSubGraph:
 
     def consumer_goods_create_sub_grapgh(self, graph: Graph, parent_node: Node):
         # Create Nodes
-        node_a = Node("Consumer Goods", "Consumer Goods")
-        node_b = Node("Text", "Google")
+        node_ConsumerGoods = Node("Consumer Goods", "Consumer Goods")
+        node_AldiNord = Node("", "")
         node_c = Node("Text", "amazon")
         node_d = Node("Text", "Tesla")
         node_e = Node("Text", "Nvidia")
+
         node_AldiNord = Node(AldiNord_Data.CONTENT, AldiNord_Data.TITEL, AldiNord_Data.IMAGE_NAME)
 
-        parent_node.connect(node_a)
-        node_a.connect(node_b)
-        node_a.connect(node_c)
-        node_a.connect(node_d)
-        node_a.connect(node_e)
-        node_a.connect(node_AldiNord)
+        parent_node.connect(node_ConsumerGoods)
+        node_ConsumerGoods.connect(node_AldiNord)
+        node_ConsumerGoods.connect(node_c)
+        node_ConsumerGoods.connect(node_d)
+        node_ConsumerGoods.connect(node_e)
+        node_ConsumerGoods.connect(node_AldiNord)
 
-        graph.add_new_node_to_graph(node_a)
-        graph.add_new_node_to_graph(node_b)
+        graph.add_new_node_to_graph(node_ConsumerGoods)
+        graph.add_new_node_to_graph(node_AldiNord)
         graph.add_new_node_to_graph(node_c)
         graph.add_new_node_to_graph(node_d)
         graph.add_new_node_to_graph(node_e)
