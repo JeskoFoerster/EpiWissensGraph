2e4e5f99d9337357591d61b88f962e4dbef015af
Noah <noah_osato.asemota@smail.th-koeln.de>
2023-11-27 14:35:04 +0100

    Added node_riotgames

--- Subgraphs/MediaAndEntertainment.py
+++ Subgraphs/MediaAndEntertainment.py
@@ -2,7 +2,7 @@ from GraphModel.Graph import Graph
 from GraphModel.Node import Node
 
 # import all the companys in the subgraph
-from Subgraphs.CompanyNodes import Netflix_Data, ESLGaming_Data
+from Subgraphs.CompanyNodes import Netflix_Data, ESLGaming_Data, RiotGames_Data
 
 
 class MediaAndEntertainmentSubGraph:
@@ -15,11 +15,14 @@ class MediaAndEntertainmentSubGraph:
         node_a = Node("media and entertainment description", "media and entertainment")
         node_netflix = Node(Netflix_Data.CONTENT, Netflix_Data.TITEL, Netflix_Data.IMAGE_NAME)
         node_eslgaming = Node(ESLGaming_Data.CONTENT, ESLGaming_Data.TITEL, ESLGaming_Data.IMAGE_NAME)
+        node_riotgames = Node(RiotGames_Data.CONTENT, RiotGames_Data.TITEL, RiotGames_Data.IMAGE_NAME)
 
         parent_node.connect(node_a)
         node_a.connect(node_netflix)
         node_a.connect(node_eslgaming)
+        node_a.connect(node_riotgames)
 
         graph.add_new_node_to_graph(node_a)
         graph.add_new_node_to_graph(node_netflix)
         graph.add_new_node_to_graph(node_eslgaming)
+        graph.add_new_node_to_graph(node_riotgames)
