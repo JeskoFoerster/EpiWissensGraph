861c1b2b06b19f00f13370c2dfb6125eb2715e8c
Nils <nils_steffen.morczinietz@th-koeln.de>
2023-11-27 19:49:45 +0100

    Google Update

--- Subgraphs/CompanyNodes/Google_Data.py
+++ Subgraphs/CompanyNodes/Google_Data.py
@@ -4,10 +4,10 @@ TITEL = "Google"
 
 CONTENT = (
 "## Beschreibung:\n"
-"Google, führend im Technologiebereich, bietet aktuell eine Vielzahl von Diensten und Produkten an. Die Suchmaschine bleibt zentral, während Gmail, Google Maps und YouTube das tägliche Leben prägen. Android, im Portfolio seit 2005, festigt die mobile Präsenz. Google treibt Innovationen voran, besonders in KI und Cloud Computing. Als Teil von Alphabet Inc. investiert Google in diverse Bereiche wie autonomes Fahren und Gesundheitswissenschaften. Die Zusammenführung von Daten und Diensten ermöglicht personalisierte Nutzererlebnisse. Mit fortschreitenden Entwicklungen in den verschiedensten Technologiefeldern bleibt Google ein Schlüsselakteur im globalen Technologiegeschehen.\n"
+"Heute ist Google ein amerikanisches multinationales Technologieunternehmen, das sich auf künstliche Intelligenz, Online-Werbung, Suchmaschinentechnologie, Cloud-Computing, Computersoftware, Quantencomputing, E-Commerce und Unterhaltungselektronik konzentriert [2]. Es wird als eines der mächtigsten Unternehmen der Welt bezeichnet. Google ist eine Tochtergesellschaft von Alphabet Inc. und ist eine Holdinggesellschaft für die Internet-Eigenschaften und -Interessen von Alphabet. Sundar Pichai wurde am 24. Oktober 2015 zum CEO von Google ernannt und ersetzte Larry Page, der zum CEO von Alphabet wurde[2]. Das Unternehmen hat seitdem schnell expandiert und bietet eine Vielzahl von Produkten und Dienstleistungen an, die über Google Search hinausgehen, viele davon haben dominante Marktanteile. Diese Produkte decken eine Vielzahl von Anwendungsfällen ab, darunter E-Mail (Gmail), Navigation (Waze & Maps), Cloud-Computing (Cloud), Web-Browsing (Chrome), Videofreigabe (YouTube), Produktivität (Workspace), Betriebssysteme (Android), Cloud-Speicher (Drive), Sprachübersetzung (Translate), Fotolagerung (Photos), Videotelefonie (Meet), Smart Home (Nest), Smartphones (Pixel), tragbare Technologie (Pixel Watch & Fitbit), Musik-Streaming (YouTube Music), Video-on-Demand (YouTube TV), künstliche Intelligenz (Google Assistant & Bard), maschinelles Lernen APIs (TensorFlow), AI-Chips (TPU) und mehr [2].\n"
 "\n"
 "## Historie:\n"
-"Google, 1998 von Larry Page und Sergey Brin gegründet, hat zahlreiche Meilensteine erreicht. Die Suchmaschine selbst war bahnbrechend, gefolgt von Gmail (2004), Google Maps (2005) und der Übernahme von YouTube (2006). Android wurde 2005 erworben und dominiert heute den mobilen Markt. Die Einführung von Chrome (2008) stärkte Googles Position im Browsersektor. Die Umstrukturierung zu Alphabet Inc. im Jahr 2015 reflektiert die Diversifizierung in verschiedene Geschäftsbereiche. Googles Fortschritte in künstlicher Intelligenz, wie DeepMind, beeinflussen die Branche\n"
+"Google wurde 1998 von Larry Page und Sergey Brin gegründet, während sie an der Stanford University in Kalifornien promovierten [1]. Sie entwickelten eine Suchmaschine namens “BackRub”, die Links verwendete, um die Bedeutung einzelner Seiten im World Wide Web zu bestimmen. Bald darauf wurde BackRub in Google umbenannt. Der Name war ein Wortspiel mit der mathematischen Bezeichnung für die Zahl 1, gefolgt von 100 Nullen, und spiegelte die Mission von Page und Brin wider, die Informationen der Welt zu organisieren und für alle zugänglich und nützlich zu machen [1]. Im August 1998 schrieb Andy Bechtolsheim, Mitbegründer von Sun Microsystems, einen Scheck über 100.000 $ aus, und Google Inc. wurde offiziell gegründet. Mit dieser Investition zog das neu gegründete Team von den Schlafsälen in ihr erstes Büro: eine Garage in Menlo Park, Kalifornien. Google wuchs schnell und zog schließlich in seinen jetzigen Hauptsitz, den Googleplex in Mountain View, Kalifornien [1] .\n"
 "\n"
 "## Mission/Ziel:\n"
 "\"Google hat es sich zum Ziel gesetzt, die Informationen der Welt zu organisieren und sie allgemein zugänglich und nutzbar zu machen. Deshalb machen wir es den Nutzern leicht, in der Google Suche ganz unterschiedliche Arten von Informationen aus einer Vielzahl von Quellen zu finden\"\n"
@@ -26,12 +26,9 @@ CONTENT = (
 "https://www.google.com/about/careers/applications/jobs/results/\n"
 "\n"
 "**Quelen:**\n"
-"*-https://de.wikipedia.org/wiki/Google_Suche\n"
-"*-https://about.google/\n"
-"\n"
-"*-https://www.google.com/intl/de/search/howsearchworks/our-approach/#:~:text=Google%20hat%20es%20sich%20zum,Vielzahl%20von%20Quellen%20zu%20finden./\n"
-"\n"
-"*-https://about.google/working/intern/?hl=de\n"
+"*-[1] https://about.google/intl/ALL_us/our-story/\n"
+"*-[2] https://en.wikipedia.org/wiki/Google\n"
+"*-[3] https://about.google/\n"
 )
 
 IMAGE_NAME = "google.png"
