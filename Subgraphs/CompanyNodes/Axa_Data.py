e6595d5c0d1aa8324d3cf328d11fddd6ee722951
Tom <tom.dominik.demuth@gmail.com>
2023-11-27 14:59:43 +0100

    Added The "Axa AG" with Title, Image and Prototype description.

---
+++ Subgraphs/CompanyNodes/Axa_Data.py
@@ -0,0 +1,7 @@
+# id:14
+
+TITEL = "Axa AG"
+
+CONTENT = ("Axa ist eine Firma")
+
+IMAGE_NAME = "axa_logo.png"
--- Subgraphs/InsuranceGraph.py
+++ Subgraphs/InsuranceGraph.py
@@ -2,7 +2,7 @@ from GraphModel.Graph import Graph
 from GraphModel.Node import Node
 
 # import all the companys in the subgraph
-from Subgraphs.CompanyNodes import AldiNord_Data
+from Subgraphs.CompanyNodes import Axa_Data
 
 
 class InsuranceSubGraph:
@@ -14,7 +14,11 @@ class InsuranceSubGraph:
         # Create Nodes
         node_Insurance = Node("Ausgewählte Firmen die Versicherungen anbieten.", "Versicherungen", "insurance.png")
 
+        node_Axa = Node(Axa_Data.CONTENT, Axa_Data.TITEL, Axa_Data.IMAGE_NAME)
+
         parent_node.connect(node_Insurance)
+        node_Insurance.connect(node_Axa)
 
         graph.add_new_node_to_graph(node_Insurance)
+        graph.add_new_node_to_graph(node_Axa)
 
