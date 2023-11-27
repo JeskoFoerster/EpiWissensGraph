14219fe52042f4456aa48f877ebecf6fddaa0ac5
Nils <nils_steffen.morczinietz@th-koeln.de>
2023-11-27 15:38:50 +0100

    Boeing DONE SpaceX Intership update

---
+++ Subgraphs/CompanyNodes/Boeing_Data.py
@@ -0,0 +1,30 @@
+TITEL = "Boeing"
+
+CONTENT = (
+"## Beschreibung:\n"
+"Boeing ist ein US-amerikanisches Unternehmen, das sich auf die Entwicklung, Herstellung und den Verkauf von Flugzeugen, Raketen und Satelliten spezialisiert hat. Das Unternehmen ist der größte Flugzeughersteller der Welt und hat seinen Hauptsitz in Chicago, Illinois. Boeing ist auch ein wichtiger Lieferant für die US-amerikanische Regierung und das US-Militär. Momentan hat Boeing mit einigen Herausforderungen zu kämpfen. Die COVID-19-Pandemie hat die Luftfahrtindustrie stark beeinträchtigt, was zu einem Rückgang der Nachfrage nach Flugzeugen geführt hat. Darüber hinaus hat Boeing Probleme mit der Produktion seines 737 MAX-Flugzeugs, das nach zwei Abstürzen im Jahr 2018 und 2019 weltweit aus dem Verkehr gezogen wurde."
+"\n"
+"## Historie:\n"
+"Boeing ist ein US-amerikanisches Unternehmen, das sich auf die Entwicklung, Herstellung und den Verkauf von Flugzeugen, Raketen und Satelliten spezialisiert hat. Das Unternehmen ist der größte Flugzeughersteller der Welt und hat seinen Hauptsitz in Chicago, Illinois. Boeing hat im Laufe seiner Geschichte viele Meilensteine erreicht."
+
+"Im Jahr 1916 gründete William E. Boeing die Pacific Aero Products Company, die später in Boeing Airline Company umbenannt wurde. Das Unternehmen produzierte das erste Flugzeug, das in den USA für den kommerziellen Passagierverkehr zugelassen wurde."
+
+"Im Jahr 1933 stellte Boeing das Modell 247 vor, das erste moderne Verkehrsflugzeug mit einer vollständig aus Metall gefertigten Karosserie."
+
+"Im Jahr 1954 führte Boeing das erste strahlgetriebene Verkehrsflugzeug, die Boeing 707, ein."
+
+"Im Jahr 1969 startete die Boeing 747, das erste Großraumflugzeug der Welt, zum ersten Mal."
+
+"Im Jahr 1988 führte Boeing die Boeing 747-400 ein, die erste Version des Flugzeugs mit einem Glascockpit.\n"
+"\n"
+"## Mission/Ziel:\n"
+"Die Mission von Boeing ist es, die Welt durch Innovation und Technologie zu verbinden. Das Unternehmen strebt danach, die Luftfahrtindustrie durch die Entwicklung von fortschrittlichen Technologien und Produkten zu revolutionieren, die die Sicherheit, Effizienz und Nachhaltigkeit des Fliegens verbessern. Boeing hat auch eine starke Verpflichtung zur Nachhaltigkeit und arbeitet daran, seine Produkte und Prozesse umweltfreundlicher zu gestalten.\n"
+"\n"
+"**Website:**\n"
+"https://www.boeing.de/\n"
+"\n"
+"**Mitarbeiterzahl:**\n"
+"156.000 (2022)\n"
+)
+
+IMAGE_NAME = "boeing.png"
