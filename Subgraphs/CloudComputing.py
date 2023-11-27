d0d4b61e8b18ae378faf0220cb6da00beb711554
Nils <nils_steffen.morczinietz@th-koeln.de>
2023-11-27 20:37:09 +0100

    Microsoft DONE

--- Subgraphs/CloudComputing.py
+++ Subgraphs/CloudComputing.py
@@ -3,6 +3,7 @@ from GraphModel.Node import Node
 
 # import all the companys in the subgraph
 # EXAMPLE from Subgraphs.CompanyNodes import SpaceX_Data
+from Subgraphs.CompanyNodes import  Microsoft_Data
 
 class CloudComputingGraph:
 
@@ -12,10 +13,10 @@ class CloudComputingGraph:
     def cloud_computing_create_sub_graph(self, graph: Graph, parent_node: Node):
         # Create Nodes
         node_cloud_computing = Node("node_cloud_computing", "node_cloud_computing")
-        node_b = Node("LEER 1", "LEER 11")
+        node_microsoft = Node(Microsoft_Data.CONTENT, Microsoft_Data.TITEL, Microsoft_Data.IMAGE_NAME)
 
         parent_node.connect(node_cloud_computing)
-        node_cloud_computing.connect(node_b)
+        node_cloud_computing.connect(node_microsoft)
 
         graph.add_new_node_to_graph(node_cloud_computing)
-        graph.add_new_node_to_graph(node_b)
+        graph.add_new_node_to_graph(node_microsoft)
