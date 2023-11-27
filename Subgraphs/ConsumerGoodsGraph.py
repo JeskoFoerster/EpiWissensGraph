8ced520ab3493fb4b54b1765083b1f9bb09f9b51
Tom <tom.dominik.demuth@gmail.com>
2023-11-27 12:28:12 +0100

    Added logo image for "Action", As well as a Prototype Text and a Title

--- Subgraphs/ConsumerGoodsGraph.py
+++ Subgraphs/ConsumerGoodsGraph.py
@@ -2,7 +2,7 @@ from GraphModel.Graph import Graph
 from GraphModel.Node import Node
 
 # import all the companys in the subgraph
-from Subgraphs.CompanyNodes import AldiNord_Data, Lidl_Data
+from Subgraphs.CompanyNodes import AldiNord_Data, Lidl_Data, Action_Data
 
 
 class ConsumerGoodsSubGraph:
@@ -13,24 +13,23 @@ class ConsumerGoodsSubGraph:
     def consumer_goods_create_sub_grapgh(self, graph: Graph, parent_node: Node):
         # Create Nodes
         node_ConsumerGoods = Node("Consumer Goods", "Consumer Goods")
-        node_AldiNord = Node("", "")
-        node_Lidl = Node("", "")
-        node_d = Node("Text", "Tesla")
+        node_AldiNord = Node("Aldi Nord", "Aldi Nord")
+        node_Lidl = Node("Lidl", "Lidl")
+        node_Action = Node("Action", "Action")
         node_e = Node("Text", "Nvidia")
 
         node_AldiNord = Node(AldiNord_Data.CONTENT, AldiNord_Data.TITEL, AldiNord_Data.IMAGE_NAME)
         node_Lidl = Node(Lidl_Data.CONTENT, Lidl_Data.TITEL, Lidl_Data.IMAGE_NAME)
+        node_Action = Node(Action_Data.CONTENT, Action_Data.TITEL, Action_Data.IMAGE_NAME)
 
         parent_node.connect(node_ConsumerGoods)
         node_ConsumerGoods.connect(node_AldiNord)
         node_ConsumerGoods.connect(node_Lidl)
-        node_ConsumerGoods.connect(node_d)
+        node_ConsumerGoods.connect(node_Action)
         node_ConsumerGoods.connect(node_e)
-        node_ConsumerGoods.connect(node_AldiNord)
 
         graph.add_new_node_to_graph(node_ConsumerGoods)
         graph.add_new_node_to_graph(node_AldiNord)
         graph.add_new_node_to_graph(node_Lidl)
-        graph.add_new_node_to_graph(node_d)
+        graph.add_new_node_to_graph(node_Action)
         graph.add_new_node_to_graph(node_e)
-        graph.add_new_node_to_graph(node_AldiNord)
