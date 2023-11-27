ed9a91abf73e2861b73c3bed7dcb2a2e780becce
eliasdick <eliasdick0804@gmail.com>
2023-11-27 16:21:31 +0100

    Added The "Axa AG" with Title, Image and Prototype description.

--- Subgraphs/CompanyNodes/ElectronicArts_Data.py
+++ Subgraphs/CompanyNodes/ElectronicArts_Data.py
@@ -2,6 +2,25 @@
 
 TITEL = "Electronic Arts"
 
-CONTENT = ("")
+CONTENT = (
+"## Beschreibung:\n"
+"Text 1\n"
+"\n"
+"## Historie:\n"
+"Text 2\n"
+"\n"
+"## Mission/Ziel:\n"
+"Text 3\n"
+"\n"
+"**Website:**\n"
+"Text 4\n"
+"\n"
+"**Mitarbeiterzahl:**\n"
+"Text 5\n"
+"\n"
+"**Praxissemester/Praktikum:**\n"
+"Ja/Nein"
+"**Kontakt:**\n"
+"Text 6\n")
 
-IMAGE_NAME = ""
+IMAGE_NAME = "electronicarts_logo.png"
