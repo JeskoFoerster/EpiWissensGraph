b21a5a75d6841c9706789be409e043941876f6fe
Jesko <jesko.foerster@coseon.com>
2023-11-27 21:19:02 +0100

    added Nestle

--- Subgraphs/CompanyNodes/Nestle_Data.py
+++ Subgraphs/CompanyNodes/Nestle_Data.py
@@ -2,6 +2,30 @@
 
 TITEL = "Nestlé S.A."
 
-CONTENT = ("Nestle ist eine Firma")
+CONTENT = ("## Beschreibung:\n"
+"Der Nestlé Konzern, als einer der weltweit größten Lebensmittel- und Getränkehersteller, steht für eine reiche Vielfalt von Marken, die in nahezu jedem Haushalt präsent sind. Mit einer umfassenden Palette von Produkten reicht die Präsenz von Nestlé von Nahrungsmitteln und Getränken bis hin zu Gesundheitswissenschaften.[1]\n"
+"\n"
+
+"## Historie:\n"
+"Die Ursprünge der Nestlé-Geschichte reichen bis ins Jahr 1866 zurück, als die AngloSwiss Condensed Milk Company von den Brüdern Charles und George Page in den USA gegründet wurde. Diese Gesellschaft, die Europas erste Produktionsstätte für Kondensmilch etablierte, fusionierte 1905 mit der von Henri Nestlé gegründeten Gesellschaft, um den Nestlé-Konzern zu formen. Im Jahr 1867 entwickelte Henri Nestlé, ein deutscher Apotheker, das bahnbrechende Babynahrungsmittel \"farine lactée\" (Mehl mit Milch) in Vevey, Schweiz. Diese Mischung aus Kuhmilch, Weizenmehl und Zucker wurde für Säuglinge konzipiert, die nicht gestillt werden konnten, um die hohe Sterblichkeitsrate zu bekämpfen. In dieser Zeit führte er auch das ikonische \"Nest\"-Logo ein. Nach verschiedenen Entwicklungen und Fusionen in den folgenden Jahrzehnten, darunter der Eintritt in den Schokoladenmarkt im Jahr 1904, erfolgte 1905 die Fusion von Anglo-Swiss und Nestlé zur Nestlé & Anglo-Swiss Milk Company. Das Unternehmen expandierte global und entwickelte sich bis zum Ausbruch des Ersten Weltkriegs 1914 zu einem weltweit agierenden Milchkonzern. Während des Krieges stellte Nestlé & Anglo-Swiss seine Produktion auf die steigende Nachfrage nach Kondensmilch und Schokolade für die Truppen um. In den 1920er Jahren überstand das Unternehmen Konkurrenzdruck, Fusionen und eine schwierige Zeit nach dem Ersten Weltkrieg. In den darauffolgenden Jahrzehnten diversifizierte sich Nestlé weiter, erwarb Unternehmen in den Bereichen gefrorene Lebensmittel, Süßwaren, Tierfutter und Kosmetik. In den 1980er Jahren führte das Unternehmen die \"Nutrition, Health and Wellness\"-Strategie ein, um den veränderten Bedürfnissen gesundheitsbewusster Verbraucher gerecht zu werden. Die Jahre 2000 bis 2016 waren durch eine verstärkte Fokussierung auf nachhaltige Lieferketten und die Einführung von Programmen wie dem Nestlé Cocoa Plan und Nescafé Plan geprägt. Nestlé trat auch in neue Bereiche wie medizinische Ernährung ein und setzte verstärkt auf gesundheitsbewusste Produkte. In den letzten Jahren hat Nestlé seine Nachhaltigkeitsbemühungen verstärkt, sich auf den Gesundheitssektor konzentriert und Akquisitionen in den Bereichen pflanzliche Ernährung, Medizinprodukte und funktionale Getränke getätigt. Die Vision von Nestlé, eine bessere Zukunft zu schaffen, spiegelt sich in seinen aktuellen Bemühungen wider, Kinderarbeit zu bekämpfen und klimaneutral zu werden.[2]\n"
+"\n"
+
+"## Mission/Ziel:\n"
+"Die Mission von Nestlé ist es, Menschen durch eine breite Palette von qualitativ hochwertigen Lebensmitteln und Getränken zu einer besseren Lebensqualität zu verhelfen. Das Unternehmen verfolgt dabei das Ziel, durch nachhaltige Praktiken und kontinuierliche Innovationen einen positiven Beitrag zur Gesellschaft und zur Umwelt zu leisten.[1]\n"
+"\n"
+
+"**Website:**\n"
+"https://www.nestle.com/\n"
+"\n"
+
+"**Mitarbeiterzahl:**\n"
+"Nestlé beschäftigt Weltweit ca 275.000 Mitarbeiter.\n"
+"\n"
+
+"**Quelle:**\n"
+"-[1]https://www.nestle.com/\n"
+"-[2]https://www.nestle.com/sites/default/files/2022-12/nestle-company-history-timeline.pdf\n"
+"-[3]https://de.statista.com/statistik/daten/studie/412617/umfrage/mitarbeiter-von-nestle-weltweit/#:~:text=Der%20Schweizer%20Nahrungsmittelhersteller%20Nestl%C3%A9%20besch%C3%A4ftigte,der%20f%C3%BChrenden%20Konsumg%C3%BCterhersteller%20der%20Welt.\n"
+"-Bild: https://freebiesupply.com/logos/nestle-logo/\n")
 
 IMAGE_NAME = "nestle_logo.png"
