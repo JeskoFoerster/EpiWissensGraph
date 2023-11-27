1c4b8efccc7e0dfb32c9b0efe840d17a5be7b21b
Jesko <jesko.foerster@coseon.com>
2023-11-27 20:44:47 +0100

    added Rewe digital

---
+++ Subgraphs/CompanyNodes/ReweDigital_Data.py
@@ -0,0 +1,30 @@
+# id:10
+
+TITEL = "Rewe digital GmbH"
+
+CONTENT = ("## Beschreibung:\n"
+"Rewe ist eine deutsche Einzelhandelsgruppe, die sich auf Lebensmittel und Supermärkte spezialisiert hat. Die Digitalisierung hat in den letzten Jahren auch die Einzelhandelsbranche erheblich beeinflusst, und Unternehmen wie Rewe haben digitale Initiativen ergriffen, um ihre Geschäftsprozesse zu optimieren und den Kunden innovative Dienstleistungen anzubieten. Daher hat sich die Rewe Group dazu entschieden die Rewedigital GmbH zu gründen. ReweDigital könnte sich auf die digitalen Aktivitäten und Initiativen von Rewe beziehen. Dies könnte die Entwicklung von Online-Shopping-Plattformen, mobile Apps, digitale Zahlungssysteme und andere technologische Lösungen einschließen, um den Kunden ein verbessertes Einkaufserlebnis zu bieten. Die Digitalisierung ermöglicht es Einzelhändlern auch, ihre Logistik- und Lieferketten effizienter zu gestalten. Weltweit betreut die Rewe-Digital GmbH 7300 Märkte, 32000 Kassen und verwaltet täglich 1. Mrd. Umsatz für die Rewe-Group[1]\n"
+"\n"
+
+"## Historie:\n"
+"Die Rewe digital AG wurde in den 1980ern gegründet, um die digitale Transformation der Rewe Group voranzutreiben.[1]\n"
+"\n"
+
+"## Mission/Ziel:\n"
+"Die Mission der Rewe digital AG besteht darin, durch digitale Innovationen das Einkaufserlebnis für Kunden zu optimieren und die Prozesse im Handel effizienter zu gestalten. Mit dem Ziel, die führende Rolle der Rewe Group im Einzelhandel weiter zu stärken, setzt die Rewe digital AG auf kundenorientierte Lösungen und zukunftsweisende Technologien.[1]\n"
+"\n"
+
+"**Website:**\n"
+"https://www.rewe-digital.com/de\n"
+"\n"
+
+"**Mitarbeiterzahl:**\n"
+"Die Firma Rewe Digital schäftigte 2022 ca. 2200 Menschen für 2023 wurden jedoch keine Daten veröffentlicht[1]\n"
+"\n"
+
+"**Quelle:**\n"
+"[1]https://www.rewe-digital.com/de\n"
+"Bild: https://commons.wikimedia.org/wiki/File:REWE_Logo_ohne_Rahmen.png"
+"\n")
+
+IMAGE_NAME = "rewe_logo.png"
--- Subgraphs/InformationTechnologyGraph.py
+++ Subgraphs/InformationTechnologyGraph.py
@@ -2,7 +2,7 @@ from GraphModel.Graph import Graph
 from GraphModel.Node import Node
 
 # import all the companys in the subgraph
-from Subgraphs.CompanyNodes import Coseon_Data, SchneiderElectric_Data, ElectronicArts_Data, Google_Data
+from Subgraphs.CompanyNodes import Coseon_Data, SchneiderElectric_Data, ElectronicArts_Data, Google_Data, ReweDigital_Data
 
 class InformationTechnologySubGraph:
 
@@ -16,15 +16,18 @@ class InformationTechnologySubGraph:
         node_coseon = Node(Coseon_Data.CONTENT, Coseon_Data.TITEL, Coseon_Data.IMAGE_NAME)
         node_electronicarts = Node(ElectronicArts_Data.CONTENT, ElectronicArts_Data.TITEL, ElectronicArts_Data.IMAGE_NAME)
         node_google = Node(Google_Data.CONTENT, Google_Data.TITEL, Google_Data.IMAGE_NAME)
+        node_rewe_digital = Node(ReweDigital_Data.CONTENT, ReweDigital_Data.TITEL, ReweDigital_Data.IMAGE_NAME)
 
         parent_node.connect(node_information_technology)
         node_information_technology.connect(node_coseon)
         node_information_technology.connect(node_schneiderelectric)
         node_information_technology.connect(node_electronicarts)
         node_information_technology.connect(node_google)
+        node_information_technology.connect(node_rewe_digital)
 
         graph.add_new_node_to_graph(node_information_technology)
         graph.add_new_node_to_graph(node_coseon)
         graph.add_new_node_to_graph(node_schneiderelectric)
         graph.add_new_node_to_graph(node_electronicarts)
         graph.add_new_node_to_graph(node_google)
+        graph.add_new_node_to_graph(node_rewe_digital)
