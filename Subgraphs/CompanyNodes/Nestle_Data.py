c1e8c4794480518fec91809b8642bb8f41f1eea9
Tom <tom.dominik.demuth@gmail.com>
2023-11-27 14:11:33 +0100

    Added a note for Nestlé with Image, Title and Prototype Description

---
+++ Subgraphs/CompanyNodes/Nestle_Data.py
@@ -0,0 +1,7 @@
+# id:7
+
+TITEL = "Nestlé S.A."
+
+CONTENT = ("Nestle ist eine Firma")
+
+IMAGE_NAME = "nestle_logo.png"
--- Subgraphs/ConsumerGoodsGraph.py
+++ Subgraphs/ConsumerGoodsGraph.py
@@ -2,7 +2,7 @@ from GraphModel.Graph import Graph
 from GraphModel.Node import Node
 
 # import all the companys in the subgraph
-from Subgraphs.CompanyNodes import AldiNord_Data, Lidl_Data, Action_Data, Obi_Data
+from Subgraphs.CompanyNodes import AldiNord_Data, Lidl_Data, Action_Data, Obi_Data, Nestle_Data
 
 
 class ConsumerGoodsSubGraph:
@@ -18,15 +18,18 @@ class ConsumerGoodsSubGraph:
         node_Lidl = Node(Lidl_Data.CONTENT, Lidl_Data.TITEL, Lidl_Data.IMAGE_NAME)
         node_Action = Node(Action_Data.CONTENT, Action_Data.TITEL, Action_Data.IMAGE_NAME)
         node_Obi = Node(Obi_Data.CONTENT, Obi_Data.TITEL, Obi_Data.IMAGE_NAME)
+        node_Nestle = Node(Nestle_Data.CONTENT, Nestle_Data.TITEL, Nestle_Data.IMAGE_NAME)
 
         parent_node.connect(node_ConsumerGoods)
         node_ConsumerGoods.connect(node_AldiNord)
         node_ConsumerGoods.connect(node_Lidl)
         node_ConsumerGoods.connect(node_Action)
         node_ConsumerGoods.connect(node_Obi)
+        node_ConsumerGoods.connect(node_Nestle)
 
         graph.add_new_node_to_graph(node_ConsumerGoods)
         graph.add_new_node_to_graph(node_AldiNord)
         graph.add_new_node_to_graph(node_Lidl)
         graph.add_new_node_to_graph(node_Action)
         graph.add_new_node_to_graph(node_Obi)
+        graph.add_new_node_to_graph(node_Nestle)
