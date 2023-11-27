0ee1aaa7c844498645ee6d3496cd8baea451fc59
Nils <nils_steffen.morczinietz@th-koeln.de>
2023-11-27 18:12:28 +0100

    clear it subgraph structure Google DONE

---
+++ Subgraphs/CompanyNodes/Google_Data.py
@@ -0,0 +1,35 @@
+# id:1
+
+TITEL = "Google"
+
+CONTENT = (
+"## Beschreibung:\n"
+"Google, führend im Technologiebereich, bietet aktuell eine Vielzahl von Diensten und Produkten an. Die Suchmaschine bleibt zentral, während Gmail, Google Maps und YouTube das tägliche Leben prägen. Android, im Portfolio seit 2005, festigt die mobile Präsenz. Google treibt Innovationen voran, besonders in KI und Cloud Computing. Als Teil von Alphabet Inc. investiert Google in diverse Bereiche wie autonomes Fahren und Gesundheitswissenschaften. Die Zusammenführung von Daten und Diensten ermöglicht personalisierte Nutzererlebnisse. Mit fortschreitenden Entwicklungen in den verschiedensten Technologiefeldern bleibt Google ein Schlüsselakteur im globalen Technologiegeschehen.\n"
+"\n"
+"## Historie:\n"
+"Google, 1998 von Larry Page und Sergey Brin gegründet, hat zahlreiche Meilensteine erreicht. Die Suchmaschine selbst war bahnbrechend, gefolgt von Gmail (2004), Google Maps (2005) und der Übernahme von YouTube (2006). Android wurde 2005 erworben und dominiert heute den mobilen Markt. Die Einführung von Chrome (2008) stärkte Googles Position im Browsersektor. Die Umstrukturierung zu Alphabet Inc. im Jahr 2015 reflektiert die Diversifizierung in verschiedene Geschäftsbereiche. Googles Fortschritte in künstlicher Intelligenz, wie DeepMind, beeinflussen die Branche\n"
+"\n"
+"## Mission/Ziel:\n"
+"\"Google hat es sich zum Ziel gesetzt, die Informationen der Welt zu organisieren und sie allgemein zugänglich und nutzbar zu machen. Deshalb machen wir es den Nutzern leicht, in der Google Suche ganz unterschiedliche Arten von Informationen aus einer Vielzahl von Quellen zu finden\"\n"
+"\n"
+"**Website:**\n"
+"https://about.google/\n"
+"\n"
+"**Mitarbeiterzahl:**\n"
+"200.000+ (Stand 2019)\n"
+"\n"
+"**Praxissemester/Praktikum:**\n"
+"Ja"
+"**Kontakt:**\n"
+"https://www.google.com/about/careers/applications/students/\n"
+"https://www.google.com/about/careers/applications/jobs/results/\n"
+"**Quelen:**\n"
+"*-https://de.wikipedia.org/wiki/Google_Suche\n"
+"*-https://about.google/\n"
+"\n"
+"*-https://www.google.com/intl/de/search/howsearchworks/our-approach/#:~:text=Google%20hat%20es%20sich%20zum,Vielzahl%20von%20Quellen%20zu%20finden./\n"
+"\n"
+"*-https://about.google/working/intern/?hl=de\n"
+)
+
+IMAGE_NAME = "google.png"
