8752e9e3d936c4fbfd468098c916803df85e7b55
Nils <nils_steffen.morczinietz@th-koeln.de>
2023-11-27 20:06:04 +0100

    Boeing Update

--- Subgraphs/CompanyNodes/Boeing_Data.py
+++ Subgraphs/CompanyNodes/Boeing_Data.py
@@ -3,20 +3,10 @@ TITEL = "Boeing"
 
 CONTENT = (
 "## Beschreibung:\n"
-"Boeing ist ein US-amerikanisches Unternehmen, das sich auf die Entwicklung, Herstellung und den Verkauf von Flugzeugen, Raketen und Satelliten spezialisiert hat. Das Unternehmen ist der größte Flugzeughersteller der Welt und hat seinen Hauptsitz in Chicago, Illinois. Boeing ist auch ein wichtiger Lieferant für die US-amerikanische Regierung und das US-Militär. Momentan hat Boeing mit einigen Herausforderungen zu kämpfen. Die COVID-19-Pandemie hat die Luftfahrtindustrie stark beeinträchtigt, was zu einem Rückgang der Nachfrage nach Flugzeugen geführt hat. Darüber hinaus hat Boeing Probleme mit der Produktion seines 737 MAX-Flugzeugs, das nach zwei Abstürzen im Jahr 2018 und 2019 weltweit aus dem Verkehr gezogen wurde."
+"Boeing ist ein führendes globales Luft- und Raumfahrtunternehmen, das kommerzielle Flugzeuge, Verteidigungsprodukte und Weltraumsysteme für Kunden in mehr als 150 Ländern entwickelt, herstellt und wartet [4]. Als einer der größten US-Exporteure nutzt das Unternehmen die Talente einer globalen Lieferantenbasis, um wirtschaftliche Chancen, Nachhaltigkeit und gesellschaftliche Auswirkungen voranzutreiben [4]. Die Geschäftseinheiten von Boeing sind um drei Hauptgruppen von Produkten und Dienstleistungen organisiert - kommerzielle Flugzeuge, Militärflugzeuge und Raketen sowie Raumfahrt und Kommunikation [1]. Boeing stellt sieben verschiedene Familien von Verkehrsflugzeugen her, die in zwei Einrichtungen - Renton und Everett - im Bundesstaat Washington montiert werden [1].\n"
 "\n"
 "## Historie:\n"
-"Boeing ist ein US-amerikanisches Unternehmen, das sich auf die Entwicklung, Herstellung und den Verkauf von Flugzeugen, Raketen und Satelliten spezialisiert hat. Das Unternehmen ist der größte Flugzeughersteller der Welt und hat seinen Hauptsitz in Chicago, Illinois. Boeing hat im Laufe seiner Geschichte viele Meilensteine erreicht."
-
-"Im Jahr 1916 gründete William E. Boeing die Pacific Aero Products Company, die später in Boeing Airline Company umbenannt wurde. Das Unternehmen produzierte das erste Flugzeug, das in den USA für den kommerziellen Passagierverkehr zugelassen wurde."
-
-"Im Jahr 1933 stellte Boeing das Modell 247 vor, das erste moderne Verkehrsflugzeug mit einer vollständig aus Metall gefertigten Karosserie."
-
-"Im Jahr 1954 führte Boeing das erste strahlgetriebene Verkehrsflugzeug, die Boeing 707, ein."
-
-"Im Jahr 1969 startete die Boeing 747, das erste Großraumflugzeug der Welt, zum ersten Mal."
-
-"Im Jahr 1988 führte Boeing die Boeing 747-400 ein, die erste Version des Flugzeugs mit einem Glascockpit.\n"
+"Die Boeing Company wurde 1916 von dem amerikanischen Holzhändler William E. Boeing gegründet [1] [2]. Boeing gründete das Unternehmen kurz nachdem er und der US-Marineoffizier Conrad Westervelt ein einmotoriges, zweisitziges Wasserflugzeug, das B&W, entwickelt hatten. Im Jahr 1917 wurde das Unternehmen in Boeing Airplane Company umbenannt1. Während des Ersten Weltkriegs baute das Unternehmen “Flugboote” für die Marine [1]. Im Jahr 1916 begann Boeing mit der Produktion von Flugzeugen, was zum ersten Flug des Model 1 führte [3]. Im Laufe der Jahre hat Boeing eine Reihe von Meilensteinen erreicht. Im Jahr 1930 baute Boeing den Monomail, ein Ganzmetall-Monoplan, der Post beförderte [2].\n"
 "\n"
 "## Mission/Ziel:\n"
 "Die Mission von Boeing ist es, die Welt durch Innovation und Technologie zu verbinden. Das Unternehmen strebt danach, die Luftfahrtindustrie durch die Entwicklung von fortschrittlichen Technologien und Produkten zu revolutionieren, die die Sicherheit, Effizienz und Nachhaltigkeit des Fliegens verbessern. Boeing hat auch eine starke Verpflichtung zur Nachhaltigkeit und arbeitet daran, seine Produkte und Prozesse umweltfreundlicher zu gestalten.\n"
@@ -26,6 +16,14 @@ CONTENT = (
 "\n"
 "**Mitarbeiterzahl:**\n"
 "156.000 (2022)\n"
+"\n"
+"**Quellen:**\n"
+"*-[1] https://www.boeing.com/history\n"
+"*-[2] https://en.wikipedia.org/wiki/History_of_Boeing\n"
+"*-[3] https://simpleflying.com/boeing-history/\n"
+"*-[4] https://www.britannica.com/topic/Boeing-Company\n"
+"*-[5] https://www.boeing.com/company/\n"
+"\n"
 )
 
 IMAGE_NAME = "boeing.png"
