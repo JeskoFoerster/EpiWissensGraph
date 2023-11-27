a51d8fdf3e4f8fabf3bec58d930160afccdbde35
Noah <noah_osato.asemota@smail.th-koeln.de>
2023-11-27 12:34:09 +0100

    Added ESLGaming Data and Image.

---
+++ Subgraphs/CompanyNodes/ESLGaming_Data.py
@@ -0,0 +1,7 @@
+# id:26
+
+TITEL = "ESL Gaming GmbH"
+
+CONTENT = ("ESL Gaming ist eine Firma, welche sich auf Esports Fokussiert.")
+
+IMAGE_NAME = "eslgameing_logo.png"
--- Subgraphs/MediaAndEntertainment.py
+++ Subgraphs/MediaAndEntertainment.py
@@ -2,7 +2,7 @@ from GraphModel.Graph import Graph
 from GraphModel.Node import Node
 
 # import all the companys in the subgraph
-from Subgraphs.CompanyNodes import Netflix_Data
+from Subgraphs.CompanyNodes import Netflix_Data, ESLGaming_Data
 
 
 class MediaAndEntertainmentSubGraph:
@@ -14,9 +14,12 @@ class MediaAndEntertainmentSubGraph:
         # Create Nodes
         node_a = Node("media and entertainment description", "media and entertainment")
         node_netflix = Node(Netflix_Data.CONTENT, Netflix_Data.TITEL, Netflix_Data.IMAGE_NAME)
+        node_eslgaming = Node(ESLGaming_Data.CONTENT, ESLGaming_Data.TITEL, ESLGaming_Data.IMAGE_NAME)
 
         parent_node.connect(node_a)
         node_a.connect(node_netflix)
+        node_a.connect(node_eslgaming)
 
         graph.add_new_node_to_graph(node_a)
         graph.add_new_node_to_graph(node_netflix)
+        graph.add_new_node_to_graph(node_eslgaming)
