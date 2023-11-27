10812ac757f47b37fe92152f94b49a74195432b5
Jesko <jesko.foerster@coseon.com>
2023-11-27 21:49:27 +0100

    added EA properly

--- Subgraphs/CompanyNodes/ElectronicArts_Data.py
+++ Subgraphs/CompanyNodes/ElectronicArts_Data.py
@@ -4,23 +4,29 @@ TITEL = "Electronic Arts"
 
 CONTENT = (
 "## Beschreibung:\n"
-"Text 1\n"
+"Electronic Arts (EA) ist ein international führendes Unternehmen im Bereich der Unterhaltungssoftware und Videospiele. Gegründet im Jahr 1982, hat sich EA zu einem der bedeutendsten Player in der Gaming-Branche entwickelt. Das Unternehmen ist bekannt für die Entwicklung und Veröffentlichung von hochwertigen Videospielen für verschiedene Plattformen, darunter PC, Konsolen und mobile Geräte.[1]\n"
 "\n"
+
 "## Historie:\n"
-"Text 2\n"
+"Electronic Arts wurde 1982 von Trip Hawkins gegründet und hat seitdem eine beeindruckende Geschichte in der Videospielindustrie geschrieben. Das Unternehmen war maßgeblich an der Entwicklung und Veröffentlichung einiger der erfolgreichsten und innovativsten Spiele beteiligt. Von der \"FIFA\"-Reihe bis zu \"The Sims\" hat EA die Gaming-Landschaft nachhaltig geprägt.[1]\n"
 "\n"
+
 "## Mission/Ziel:\n"
-"Text 3\n"
+"Die Mission von Electronic Arts besteht darin, durch kreative und innovative Unterhaltung Menschen weltweit zu begeistern. Das Unternehmen strebt danach, erstklassige Spielerlebnisse zu schaffen und dabei die Grenzen der Technologie und des Storytellings zu erweitern. EA setzt sich für Vielfalt, Inklusion und Nachhaltigkeit in der Spieleindustrie ein und strebt ständig danach, die Erwartungen der Spieler zu übertreffen.[1, 2]\n"
 "\n"
+
 "**Website:**\n"
-"Text 4\n"
+"https://www.ea.com/de-de\n"
 "\n"
+
 "**Mitarbeiterzahl:**\n"
-"Text 5\n"
+"Electronic Arts hatte am Ende des Geschäftsjahres 2023 (31. März 2023) auf ungefär 13400 Mitarbeiter.\n"
 "\n"
-"**Praxissemester/Praktikum:**\n"
-"Ja/Nein"
-"**Kontakt:**\n"
-"Text 6\n")
+
+"**Quellen:**\n"
+"-[1] https://www.ea.com/de-de\n"
+"-[2] https://www.ea.com/de-de/commitments/people-and-culture\n"
+"-[3] https://de.statista.com/statistik/daten/studie/1277756/umfrage/anzahl-der-mitarbeiter-von-electronic-arts-weltweit/#:~:text=Am%20Ende%20des%20Gesch%C3%A4ftsjahres%202023,z%C3%A4hlte%20das%20Unternehmen%2011.000%20Mitarbeiter.\n"
+"-Bild https://1000logos.net/ea-electronic-arts-logo/")
 
 IMAGE_NAME = "electronicarts_logo.png"
