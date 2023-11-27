03abec8acb56eb8d2e92e56608680e8f6f9e366b
Jesko <jesko.foerster@coseon.com>
2023-11-27 19:52:27 +0100

    added Axa properly

--- Subgraphs/CompanyNodes/Axa_Data.py
+++ Subgraphs/CompanyNodes/Axa_Data.py
@@ -2,6 +2,28 @@
 
 TITEL = "Axa AG"
 
-CONTENT = ("Axa ist eine Firma")
+CONTENT = ("## Beschreibung:\n"
+"Die AXA AG zählt zu den führenden Unternehmen im globalen Versicherungs- und Finanzdienstleistungssektor. Mit einer breiten Palette an innovativen Produkten und einem klaren Fokus auf Kundenzufriedenheit hat sich die AXA als vertrauenswürdiger Partner für individuelle Absicherung und finanzielle Planung etabliert.\n"
+"\n"
+
+"## Historie:\n"
+"Die AXA Konzern AG hat ihre Wurzeln in der Kölnischen Feuer-Versicherungs-Gesellschaft, auch bekannt als Colonia, die im Jahr 1839 von angesehenen Kaufleuten unter der Führung des Bankhauses Sal. Oppenheim jr. & Cie. gegründet wurde. Schon früh zeichnete sich Colonia durch Pioniergeist aus, indem sie als erstes Unternehmen Schäden, die durch Löscharbeiten verursacht wurden, in ihren Feuerpolicen einschloss. Die Colonia wuchs im Laufe der Jahre, diversifizierte ihre Angebote und gründete in den zwanziger Jahren die Rheinische Interessengemeinschaft, um dem steigenden Konkurrenzdruck zu begegnen. In den Jahren 1969 bis 1971 fusionierte Colonia mit namhaften deutschen Versicherungen, darunter die Concordia, und wurde zur Colonia Versicherung. Die Herausforderungen der deutschen Wiedervereinigung und die wachsende europäische Integration führten dazu, dass die Colonia 1991 eine Holdingstruktur bildete und sich 1997 der AXA Gruppe anschloss. Nach der Integration der Nordstern-Gesellschaften und der Albingia in den 90er-Jahren sowie der Übernahme der Winterthur Gruppe im Jahr 2006 festigte die AXA ihre Position im deutschen Versicherungsmarkt. Der AXA Konzern AG ist eines der größten Versicherungs- und Finanzdienstleistungsunternehmen der Welt.\n"
+"\n"
+
+"## Mission/Ziel:\n"
+"Die Mission der AXA AG besteht darin, ihren Kunden umfassenden Schutz und finanzielle Sicherheit zu bieten. Mit dem Ziel, die individuellen Bedürfnisse der Kunden zu verstehen und zu erfüllen, setzt die AXA auf nachhaltige und innovative Ansätze. Die langfristigen Ziele des Unternehmens sind darauf ausgerichtet, einen positiven Einfluss auf das Leben seiner Kunden auszuüben.\n"
+"\n"
+
+"**Website:**\n"
+"https://www.axa.de/\n"
+"\n"
+
+"**Mitarbeiterzahl:**\n"
+"Die AXA AG beschäftigt weltweit über 160.000 Mitarbeiter.\n"
+"\n"
+
+"**Quelle:**\n"
+"https://www.axa.de/\n"
+"\n")
 
 IMAGE_NAME = "axa_logo.png"
