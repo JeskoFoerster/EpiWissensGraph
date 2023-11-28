79b63d70d3f256cea9af463b94aff826cb63b2ed
Nils <nils_steffen.morczinietz@th-koeln.de>
2023-11-28 08:55:01 +0100

    Bmw Done

--- Subgraphs/AutomotiveIndustry.py
+++ Subgraphs/AutomotiveIndustry.py
@@ -4,6 +4,7 @@ from GraphModel.Node import Node
 # import all the companys in the subgraph
 # EXAMPLE from Subgraphs.CompanyNodes import SpaceX_Data
 from Subgraphs.CompanyNodes import  Tesla_Data
+from Subgraphs.CompanyNodes import  Bmw_Data
 
 class AutomotiveIndustry:
 
@@ -14,9 +15,12 @@ class AutomotiveIndustry:
         # Create Nodes
         node_automotive_industry = Node("node_automotive_industry", "node_automotive_industry")
         node_tesla = Node(Tesla_Data.CONTENT, Tesla_Data.TITEL, Tesla_Data.IMAGE_NAME)
+        node_bmw = Node(Bmw_Data.CONTENT, Bmw_Data.TITEL, Bmw_Data.IMAGE_NAME)
 
         parent_node.connect(node_automotive_industry)
         node_automotive_industry.connect(node_tesla)
+        node_automotive_industry.connect(node_bmw)
 
         graph.add_new_node_to_graph(node_automotive_industry)
         graph.add_new_node_to_graph(node_tesla)
+        graph.add_new_node_to_graph(node_bmw)
---
+++ Subgraphs/CompanyNodes/Bmw_Data.py
@@ -0,0 +1,38 @@
+# id:62
+
+TITEL = "BMW"
+
+CONTENT = (
+"## Beschreibung:\n"
+"Die Bayerische Motoren Werke AG, besser bekannt als BMW, ist ein multinationaler Automobil- und Motorradhersteller mit Hauptsitz in München, Deutschland [1]. Das Unternehmen wurde am 7. März 1916 gegründet und hat seine Wurzeln in der Flugzeugmotorenproduktion [2]. Heute ist die BMW Group mit ihren Marken BMW, MINI, Rolls-Royce und BMW Motorrad einer der weltweit führenden Premiumanbieter von Automobilen und Motorrädern sowie Anbieter von Premium-Finanz- und Mobilitätsdienstleistungen [3].\n"
+"\n"
+"## Historie:\n"
+"Die Geschichte von BMW beginnt im Jahr 1916, als das Unternehmen von Karl Rapp und Gustav Otto gegründet wurde. Ursprünglich als Hersteller von Flugzeugmotoren gegründet, produzierte BMW von 1917 bis 1918 und erneut von 1933 bis 1945 Flugzeugmotoren [1].\n"
+
+"Im Jahr 1923 kündigte BMW sein erstes Motorrad, die R 32, an. Bis dahin hatte das Unternehmen nur Motoren und keine kompletten Fahrzeuge geliefert. 1928 begann BMW mit der Produktion von Automobilen [1].\n"
+
+"Nach dem Zweiten Weltkrieg begann BMW mit der Produktion von Haushaltsgeräten, da das Unternehmen als Rüstungsbetrieb eingestuft wurde und Maschinen und Werkzeuge demontiert wurden. 1951 brachte BMW den BMW 501 auf den Markt, das erste Nachkriegsautomobil des Unternehmens [1].\n"
+"\n"
+"## Mission/Ziel:\n"
+"Die Mission von BMW ist es, außergewöhnliche Kundenerlebnisse zu bieten, indem sie Menschen mit einzigartigen Produkten und Dienstleistungen inspirieren, die ihre Leidenschaft für Innovation, Design und Nachhaltigkeit widerspiegeln. BMW betont seine Hingabe, außergewöhnliche Kundenerlebnisse zu bieten [2].\n"
+"\n"
+"**Website:**\n"
+"https://www.bmwgroup.com/de.html\n"
+"\n"
+"**Mitarbeiterzahl:**\n"
+"Im Geschäftsjahr 2022 beschäftigte die BMW Group weltweit etwa 149.475 Mitarbeiter [1].\n"
+"\n"
+"**Praxissemester/Praktikum:**\n"
+"Ja\n"
+"\n"
+"**Kontakt:**\n"
+"https://www.bmwgroup.jobs/de/de/studierende/praktikum.html\n"
+"\n"
+"**Quellen:**\n"
+"*-[Image] https://de.m.wikipedia.org/wiki/Datei:BMW_logo_%28gray%29.svg\n"
+"*-[1] https://www.bmwgroup.com/en/company.html\n"
+"*-[2] https://www.bmwgroup.com/de/unternehmen/strategie.html\n"
+"*-[2] https://www.comparably.com/companies/bmw/mission\n"
+)
+
+IMAGE_NAME = "bmw.png"
