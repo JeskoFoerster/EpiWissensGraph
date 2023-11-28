d85d63d47a0d4a9f146d4ada2a5bcae9faa2f0f7
Nils <nils_steffen.morczinietz@th-koeln.de>
2023-11-28 10:03:00 +0100

    Volkswagen ONE

--- Subgraphs/AutomotiveIndustry.py
+++ Subgraphs/AutomotiveIndustry.py
@@ -3,7 +3,7 @@ from GraphModel.Node import Node
 
 # import all the companys in the subgraph
 # EXAMPLE from Subgraphs.CompanyNodes import SpaceX_Data
-from Subgraphs.CompanyNodes import  Tesla_Data, Bmw_Data, Porsche_Data, Audi_Data
+from Subgraphs.CompanyNodes import  Tesla_Data, Bmw_Data, Porsche_Data, Audi_Data, Volkswagen_Data
 class AutomotiveIndustry:
 
     def __init__(self, parent_node: Node, graph: Graph):
@@ -16,15 +16,18 @@ class AutomotiveIndustry:
         node_bmw = Node(Bmw_Data.CONTENT, Bmw_Data.TITEL, Bmw_Data.IMAGE_NAME)
         node_porsche = Node(Porsche_Data.CONTENT, Porsche_Data.TITEL, Porsche_Data.IMAGE_NAME)
         node_audi = Node(Audi_Data.CONTENT, Audi_Data.TITEL, Audi_Data.IMAGE_NAME)
+        node_volkswagen = Node(Volkswagen_Data.CONTENT, Volkswagen_Data.TITEL, Volkswagen_Data.IMAGE_NAME)
 
         parent_node.connect(node_automotive_industry)
         node_automotive_industry.connect(node_tesla)
         node_automotive_industry.connect(node_bmw)
         node_automotive_industry.connect(node_porsche)
         node_automotive_industry.connect(node_audi)
+        node_automotive_industry.connect(node_volkswagen)
 
         graph.add_new_node_to_graph(node_automotive_industry)
         graph.add_new_node_to_graph(node_tesla)
         graph.add_new_node_to_graph(node_bmw)
         graph.add_new_node_to_graph(node_porsche)
         graph.add_new_node_to_graph(node_audi)
+        graph.add_new_node_to_graph(node_volkswagen)
