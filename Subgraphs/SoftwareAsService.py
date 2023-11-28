995c12cab39c82ebc4cd9c0a70d6e9b3c42a704a
Nils <nils_steffen.morczinietz@th-koeln.de>
2023-11-28 17:52:41 +0100

    sap done

--- Subgraphs/SoftwareAsService.py
+++ Subgraphs/SoftwareAsService.py
@@ -3,7 +3,7 @@ from GraphModel.Node import Node
 
 # import all the companys in the subgraph
 # EXAMPLE from Subgraphs.CompanyNodes import SpaceX_Data
-
+from Subgraphs.CompanyNodes import SAP_Data
 class SoftwareAsServiceGraph:
 
     def __init__(self, parent_node: Node, graph: Graph):
@@ -12,10 +12,10 @@ class SoftwareAsServiceGraph:
     def software_as_service_create_sub_graph(self, graph: Graph, parent_node: Node):
         # Create Nodes
         node_software_as_service = Node("node_software_as_service", "node_software_as_service")
-        node_b = Node("LEER 1", "LEER 11")
+        node_sap = Node(SAP_Data.CONTENT, SAP_Data.TITEL, SAP_Data.IMAGE_NAME)
 
         parent_node.connect(node_software_as_service)
-        node_software_as_service.connect(node_b)
+        node_software_as_service.connect(node_sap)
 
         graph.add_new_node_to_graph(node_software_as_service)
-        graph.add_new_node_to_graph(node_b)
+        graph.add_new_node_to_graph(node_sap)
