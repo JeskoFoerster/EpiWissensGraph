985585b5fe11e9b3daba1efa958db846e4dd1fb7
Nils <nils_steffen.morczinietz@th-koeln.de>
2023-11-28 18:18:21 +0100

    secutive done 2 (sry)

--- Subgraphs/Cybersecurity.py
+++ Subgraphs/Cybersecurity.py
@@ -3,7 +3,7 @@ from GraphModel.Node import Node
 
 # import all the companys in the subgraph
 # EXAMPLE from Subgraphs.CompanyNodes import SpaceX_Data
-
+from Subgraphs.CompanyNodes import  Secunet_Data
 class CybersecurityGraph:
 
     def __init__(self, parent_node: Node, graph: Graph):
@@ -12,10 +12,10 @@ class CybersecurityGraph:
     def cybersecurity_create_sub_graph(self, graph: Graph, parent_node: Node):
         # Create Nodes
         node_cybersecurity = Node("node_cybersecurity", "node_cybersecurity")
-        node_b = Node("LEER 1", "LEER 11")
+        node_secunet = Node(Secunet_Data.CONTENT, Secunet_Data.TITEL, Secunet_Data.IMAGE_NAME)
 
         parent_node.connect(node_cybersecurity)
-        node_cybersecurity.connect(node_b)
+        node_cybersecurity.connect(node_secunet)
 
         graph.add_new_node_to_graph(node_cybersecurity)
-        graph.add_new_node_to_graph(node_b)
+        graph.add_new_node_to_graph(node_secunet)
