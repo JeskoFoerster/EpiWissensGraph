977e7590ddaf35efb9da41e07692bba7715dcb0c
Jesko <jesko.foerster@coseon.com>
2023-11-29 16:12:56 +0100

    added why Innovas is interesting

--- Subgraphs/CompanyNodes/Innovas_Data.py
+++ Subgraphs/CompanyNodes/Innovas_Data.py
@@ -2,29 +2,44 @@
 TITEL = "Innovas GmbH"
 
 CONTENT = (
-"## Beschreibung:\n"
-"Die Innovas GmbH ist ein renommiertes Unternehmen, das sich auf innovative Lösungen im Bereich der Software innerhalb der Medizinverwaltung spezialisiert hat. [1]"
-"\n"
-
-"## Historie:\n"
-"Die Innovas GmbH wurde im Jahr 2018 gegründet und hat seitdem eine beeindruckende Entwicklung durchlaufen. Unsere Geschichte ist geprägt von kontinuierlichem Wachstum, erfolgreichen Partnerschaften und wegweisenden Projekten. Das System \"Kolumbus\" wird mitlerweile von 150 Geschäftlichen kunden genutzt. Kolumbus ermöglicht es Krankenhausmanagment, Krankenhausdatenaustausch, Pflegefallmanagement und Versorgungsmanagement in einer Software zu vereinen. [1]"
-"\n"
-
-"## Mission/Ziel:\n"
-"Die Mission von Innovas ist es, Unternehmen bei ihrer digitalen Transformation zu unterstützen und durch maßgeschneiderte IT-Lösungen Mehrwert zu schaffen. Die Innovas strebt danach, wegweisende Technologien zu implementieren und ihren Kunden dabei zu helfen, in einer zunehmend digitalisierten Welt erfolgreich zu sein.[2]\n"
-"\n"
-
-"**Website:**\n"
-"https://www.innovas.de/\n"
-"\n"
-
-"**Mitarbeiterzahl:**\n"
-"200 Mitarbeiter innerhalb Deutschlands.\n"
-
-"**Quelle:**\n"
-"-[1]https://www.innovas.de/\n"
-"-[2]Gespräch mit Herrn Jörg Riebschläger\n"
-"-Bild: https://www.innovas.de/"
+    "## Beschreibung:\n"
+    "Die Innovas GmbH ist ein renommiertes Unternehmen, das sich auf innovative Lösungen im Bereich der Software "
+    "innerhalb der Medizinverwaltung spezialisiert hat. [1] "
+    "\n"
+    
+    "## Warum ist Coseon für Informatiker interessant?:\n"
+    "Die Innovas GmbH präsentiert sich als äußerst attraktiver Arbeitgeber für Informatiker, insbesondere jene, "
+    "die ihre Fähigkeiten in der Medizinsoftwareentwicklung entfalten möchten. Als renommiertes Unternehmen hat sich "
+    "Innovas auf innovative Lösungen im Bereich der Software innerhalb der Medizinverwaltung spezialisiert, "
+    "wodurch es zu einem Schlüsselakteur in diesem wichtigen Sektor geworden ist.\n "
+    "\n"
+    
+    "## Historie:\n"
+    "Die Innovas GmbH wurde im Jahr 2018 gegründet und hat seitdem eine beeindruckende Entwicklung durchlaufen. "
+    "Unsere Geschichte ist geprägt von kontinuierlichem Wachstum, erfolgreichen Partnerschaften und wegweisenden "
+    "Projekten. Das System \"Kolumbus\" wird mitlerweile von 150 Geschäftlichen kunden genutzt. Kolumbus ermöglicht "
+    "es Krankenhausmanagment, Krankenhausdatenaustausch, Pflegefallmanagement und Versorgungsmanagement in einer "
+    "Software zu vereinen. [1] "
+    "\n"
+    
+    "## Mission/Ziel:\n"
+    "Die Mission von Innovas ist es, Unternehmen bei ihrer digitalen Transformation zu unterstützen und durch "
+    "maßgeschneiderte IT-Lösungen Mehrwert zu schaffen. Die Innovas strebt danach, wegweisende Technologien zu "
+    "implementieren und ihren Kunden dabei zu helfen, in einer zunehmend digitalisierten Welt erfolgreich zu sein.["
+    "2]\n "
+    "\n"
+    
+    "**Website:**\n"
+    "https://www.innovas.de/\n"
+    "\n"
+    
+    "**Mitarbeiterzahl:**\n"
+    "200 Mitarbeiter innerhalb Deutschlands.\n"
+    
+    "**Quelle:**\n"
+    "-[1]https://www.innovas.de/\n"
+    "-[2]Gespräch mit Herrn Jörg Riebschläger\n"
+    "-Bild: https://www.innovas.de/"
 )
 
 IMAGE_NAME = "innovas_logo.png"
