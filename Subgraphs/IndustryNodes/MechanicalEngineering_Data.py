ef71408bbee8561fe9d90e91df5f68b8daf44ee8
Noah <noah_osato.asemota@smail.th-koeln.de>
2023-11-29 18:54:08 +0100

    Created MechanicalEngineering_Data.py

---
+++ Subgraphs/IndustryNodes/MechanicalEngineering_Data.py
@@ -0,0 +1,9 @@
+TITEL = "Maschinenbau"
+
+CONTENT = (
+"## Beschreibung:\n"
+"Maschinenbau\n"
+"## Quellen:\n"
+"Bild:\n")
+
+IMAGE_NAME = "image_placeholder.png"
--- Subgraphs/MechanicalEngineering.py
+++ Subgraphs/MechanicalEngineering.py
@@ -3,6 +3,7 @@ from GraphModel.Node import Node
 
 # import all the companys in the subgraph
 from Subgraphs.CompanyNodes import PferdWerkzeuge_Data
+from Subgraphs.IndustryNodes import MechanicalEngineering_Data
 
 class MechanicalEngineeringGraph:
 
@@ -11,7 +12,7 @@ class MechanicalEngineeringGraph:
 
     def mechanical_engineering_create_sub_graph(self, graph: Graph, parent_node: Node):
         # Create Nodes
-        node_mechanical_engineering = Node("Maschienbau/Metallverarbeitung", "Maschienbau/Metallverarbeitung", radius=8, color=[204, 51, 255])
+        node_mechanical_engineering = Node(MechanicalEngineering_Data.CONTENT,MechanicalEngineering_Data.TITEL,MechanicalEngineering_Data.IMAGE_NAME, radius=8, color=[204, 51, 255])
         node_pferd = Node(PferdWerkzeuge_Data.CONTENT, PferdWerkzeuge_Data.TITEL, PferdWerkzeuge_Data.IMAGE_NAME)
 
         parent_node.connect(node_mechanical_engineering)
