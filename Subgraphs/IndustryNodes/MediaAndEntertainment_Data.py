3912b5c6ae8442e90d5df2f5e777f22b2dce873e
Noah <noah_osato.asemota@smail.th-koeln.de>
2023-11-29 12:29:52 +0100

    Updated MediaAndEntertainmentGraph.py with new arg Added Image

---
+++ Subgraphs/IndustryNodes/MediaAndEntertainment_Data.py
@@ -0,0 +1,9 @@
+TITEL = "MediaAndEntertainment"
+
+CONTENT = (
+"## Beschreibung:\n"
+"Media and Entertainment\n"
+"## Quellen:\n"
+"Bild: https://www.klipartz.com/en/search?q=technology")
+
+IMAGE_NAME = "mediaandentertainment_logo.png"
--- Subgraphs/MediaAndEntertainmentGraph.py
+++ Subgraphs/MediaAndEntertainmentGraph.py
@@ -3,7 +3,7 @@ from GraphModel.Node import Node
 
 # import all the companys in the subgraph
 from Subgraphs.CompanyNodes import Netflix_Data, ESLGaming_Data, RiotGames_Data
-
+from Subgraphs.IndustryNodes import MediaAndEntertainment_Data
 
 class MediaAndEntertainmentSubGraph:
 
@@ -12,17 +12,17 @@ class MediaAndEntertainmentSubGraph:
 
     def media_and_entertainment_create_sub_graph(self, graph: Graph, parent_node: Node):
         # Create Nodes
-        node_a = Node("media and entertainment description", "media and entertainment")
+        node_media_and_entertainment = Node(MediaAndEntertainment_Data.CONTENT, MediaAndEntertainment_Data.TITEL, MediaAndEntertainment_Data.IMAGE_NAME, radius=8, color=[204, 51, 255])
         node_netflix = Node(Netflix_Data.CONTENT, Netflix_Data.TITEL, Netflix_Data.IMAGE_NAME)
         node_eslgaming = Node(ESLGaming_Data.CONTENT, ESLGaming_Data.TITEL, ESLGaming_Data.IMAGE_NAME)
         node_riotgames = Node(RiotGames_Data.CONTENT, RiotGames_Data.TITEL, RiotGames_Data.IMAGE_NAME)
 
-        parent_node.connect(node_a)
-        node_a.connect(node_netflix)
-        node_a.connect(node_eslgaming)
-        node_a.connect(node_riotgames)
+        parent_node.connect(node_media_and_entertainment)
+        node_media_and_entertainment.connect(node_netflix)
+        node_media_and_entertainment.connect(node_eslgaming)
+        node_media_and_entertainment.connect(node_riotgames)
 
-        graph.add_new_node_to_graph(node_a)
+        graph.add_new_node_to_graph(node_media_and_entertainment)
         graph.add_new_node_to_graph(node_netflix)
         graph.add_new_node_to_graph(node_eslgaming)
         graph.add_new_node_to_graph(node_riotgames)
