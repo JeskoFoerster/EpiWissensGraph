0601ec0e1cada104325559d77dd6cb1adeb4540b
Noah <noah_osato.asemota@smail.th-koeln.de>
2023-11-29 18:42:46 +0100

    Created PharmaceuticalsAndHealth_Data.py

---
+++ Subgraphs/IndustryNodes/PharmaceuticalsAndHealth_Data.py
@@ -0,0 +1,9 @@
+TITEL = "Gesundheitswesen"
+
+CONTENT = (
+"## Beschreibung:\n"
+"Foobar\n"
+"## Quellen:\n"
+"Bild: https://www.freepik.com/free-vector/tiny-pharmacist-with-pills-vitamins-flat-vector-illustration-doctors-writing-prescriptions-antibiotics-working-together-helping-patients-cure-pharmacy-business-drugstore-concept_24644990.htm# \n  Image by pch.vector on Freepik \n")
+
+IMAGE_NAME = "pharmaceuticalsandhealth_logo.png"
