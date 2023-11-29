0811db17d080c9a98b50e65600feb9ab825b5c43
Noah <noah_osato.asemota@smail.th-koeln.de>
2023-11-29 19:21:50 +0100

    Created CloudComputing_Data.py

--- Subgraphs/CloudComputing.py
+++ Subgraphs/CloudComputing.py
@@ -4,7 +4,7 @@ from GraphModel.Node import Node
 # import all the companys in the subgraph
 # EXAMPLE from Subgraphs.CompanyNodes import SpaceX_Data
 from Subgraphs.CompanyNodes import  Microsoft_Data
-
+from Subgraphs.IndustryNodes import CloudComputing_Data
 class CloudComputingGraph:
 
     def __init__(self, parent_node: Node, graph: Graph):
@@ -12,7 +12,7 @@ class CloudComputingGraph:
 
     def cloud_computing_create_sub_graph(self, graph: Graph, parent_node: Node):
         # Create Nodes
-        node_cloud_computing = Node("node_cloud_computing", "node_cloud_computing", radius=8, color=[204, 51, 255])
+        node_cloud_computing = Node(CloudComputing_Data.CONTENT,CloudComputing_Data.TITEL,CloudComputing_Data.IMAGE_NAME, radius=8, color=[204, 51, 255])
         node_microsoft = Node(Microsoft_Data.CONTENT, Microsoft_Data.TITEL, Microsoft_Data.IMAGE_NAME)
 
         parent_node.connect(node_cloud_computing)
