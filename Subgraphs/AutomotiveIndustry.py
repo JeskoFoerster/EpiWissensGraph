95d54fcf5d858422724108c62b3568435988827a
Noah <noah_osato.asemota@smail.th-koeln.de>
2023-11-29 19:26:10 +0100

    Created AutomotiveIndustry_Data.py

--- Subgraphs/AutomotiveIndustry.py
+++ Subgraphs/AutomotiveIndustry.py
@@ -4,6 +4,7 @@ from GraphModel.Node import Node
 # import all the companys in the subgraph
 # EXAMPLE from Subgraphs.CompanyNodes import SpaceX_Data
 from Subgraphs.CompanyNodes import  Tesla_Data, Bmw_Data, Porsche_Data, Audi_Data, Volkswagen_Data, MercedesBenz_Data
+from Subgraphs.IndustryNodes import AutomotiveIndustry_Data
 class AutomotiveIndustry:
 
     def __init__(self, parent_node: Node, graph: Graph):
@@ -11,7 +12,7 @@ class AutomotiveIndustry:
 
     def automotive_industry_sub_graph(self, graph: Graph, parent_node: Node):
         # Create Nodes
-        node_automotive_industry = Node("node_automotive_industry", "node_automotive_industry", radius=8, color=[204, 51, 255])
+        node_automotive_industry = Node(AutomotiveIndustry_Data.CONTENT,AutomotiveIndustry_Data.TITEL,AutomotiveIndustry_Data.IMAGE_NAME, radius=8, color=[204, 51, 255])
         node_tesla = Node(Tesla_Data.CONTENT, Tesla_Data.TITEL, Tesla_Data.IMAGE_NAME)
         node_bmw = Node(Bmw_Data.CONTENT, Bmw_Data.TITEL, Bmw_Data.IMAGE_NAME)
         node_porsche = Node(Porsche_Data.CONTENT, Porsche_Data.TITEL, Porsche_Data.IMAGE_NAME)
