b3a5d139b3a6bb52b38f617ecd4615b5635b20fe
Jesko <jesko.foerster@coseon.com>
2023-11-29 19:52:14 +0100

    reformated const files

--- Subgraphs/CompanyNodes/ESLGaming_Data.py
+++ Subgraphs/CompanyNodes/ESLGaming_Data.py
@@ -2,6 +2,6 @@
 
 TITEL = "ESL Gaming GmbH"
 
-CONTENT = ("ESL Gaming ist eine Firma, welche sich auf Esports Fokussiert.")
+CONTENT = "ESL Gaming ist eine Firma, welche sich auf Esports Fokussiert."
 
 IMAGE_NAME = "eslgaming_logo.png"
