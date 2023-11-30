687e2bffd88e24c74c379d512134d4eed0f35794
Noah <noah_osato.asemota@smail.th-koeln.de>
2023-11-30 19:57:56 +0100

    filled out PharmaceuticalsAndHealth_Data.py

--- IndustryNodes/PharmaceuticalsAndHealth_Data.py
+++ IndustryNodes/PharmaceuticalsAndHealth_Data.py
@@ -2,12 +2,17 @@ TITEL = "Gesundheitswesen"
 
 CONTENT = (
     "## Beschreibung:\n"
-    "Text 1\n"
-    "\n"
+    "Die IT spielt auch im Gesundheitswesen eine wichtige Rolle. Wir zeigen Ihnen verschiedene Unternehmen\n"
+    "In denen sie im Gesundheitswesen ihren Platz finden können."
+    "Wir haben verschiedene Unternehmen angeschaut und haben geeignete rausgenommen und diese hier veranschaulicht\n"
+    "Diese Unternehmen, Jung & Alt bieten attraktive Arbeitsumgebungen wo sie als ITler and Digitalen\n"
+    "und Innovativen Lösungen Arbeiten können.\n "
+    "Wenn sie an wegweisender Technologie Arbeiten wollen, welche praktische Anwendung im Alltag findet, \n"
+    "dann schauen Sie sich diese Unternehmen an.\n"
     "**Quellen:**\n"
     "*-[Image]https://www.freepik.com/free-vector/tiny-pharmacist-with-pills-vitamins-flat-vector-illustration-doctors-writing-prescriptions-antibiotics-working-together-helping-patients-cure-pharmacy-business-drugstore-concept_24644990.htm#\n"
-    "*-[1] www.....\n"
-    "*-[2] www.....\n"
+    "*-[1] https://www.innovas.de/\n"
+    "*-[2] https://www.zeiss.de/home/überuns\n"
 )
 
 IMAGE_NAME = "pharmaceuticalsandhealth_logo.png"
