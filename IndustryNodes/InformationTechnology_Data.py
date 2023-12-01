ee550def5a58a397f160e16b12b62e75f6e7451b
Tom <tom.dominik.demuth@gmail.com>
2023-12-01 12:14:03 +0100

    Added A Description to InfoTec

--- IndustryNodes/InformationTechnology_Data.py
+++ IndustryNodes/InformationTechnology_Data.py
@@ -1,13 +1,16 @@
-TITEL = "InformationsTechnologie"
+TITEL = "Informationstechnologie"
 
 CONTENT = (
     "## Beschreibung:\n"
-    "Text 1\n"
+    "Die Informationstechnologie (IT) in einem Unternehmen erstreckt sich über die Installation und Wartung von Computernetzwerk-Systemen hinaus. Die IT umfasst sämtliche Aspekte der Datenverarbeitung, von der Untersuchung und Konzeptfindung bis zur Entwicklung, Ausführung und Unterstützung von Informationssystemen. Der Begriff \"IT\" bezieht sich sowohl auf die Systeme selbst, einschließlich Softwareanwendungen und physischer Hardware, als auch auf die IT-Governance, die sicherstellt, dass Informationstechnologien effektiv eingesetzt werden, um Unternehmensziele zu erreichen und IT-Risiken zu managen."
+    "\n\n"
+    "Die IT ist in nahezu allen Branchen und Unternehmen präsent, selbst wenn diese keine eigene IT-Abteilung haben. Seit den Anfängen des digitalen Zeitalters ist die IT ein essenzieller Aspekt des Geschäfts, der jedoch mit der zunehmenden Technologiekomplexität immer vielseitiger und komplexer geworden ist. Heutige IT umfasst Telekommunikation, Sicherheit, Cloud-Computing, Rechenzentren, Netzwerke, Speicher, Computer und Server."
+    "\n\n"
+    "Verschiedene Rollen und Zuständigkeiten sind mit der Informationstechnologie verbunden, darunter IT-Leitung (CIO/CTO oder Vice President), Systems Engineering Manager, IT-Projektmanager, Geschäftsanalyst, Netzwerkadministrator, Hardwaretechniker und Help Desk-Analysten, die für die reibungslose Funktion und Entwicklung der IT-Infrastruktur verantwortlich sind. [1]\n"
     "\n"
     "**Quellen:**\n"
-    "*-[Image]https://www.klipartz.com/en/search?q=technology"
-    "*-[1] www.....\n"
-    "*-[2] www.....\n"
+    "-[Image]https://www.klipartz.com/en/search?q=technology\n"
+    "- https://www.servicenow.com/de/products/itsm/what-is-information-technology.html [1]\n"
 )
 
 IMAGE_NAME = "it.png"
