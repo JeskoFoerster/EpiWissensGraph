6764d9209a3c3059d98c193cd4f9d9be4fa05237
Noah <noah_osato.asemota@smail.th-koeln.de>
2023-12-01 14:22:04 +0100

    filled out Financialservices added image

--- IndustryNodes/FinancialServices_Data.py
+++ IndustryNodes/FinancialServices_Data.py
@@ -2,12 +2,17 @@ TITEL = "Finanz Dienstleistungen"
 
 CONTENT = (
     "## Beschreibung:\n"
-    "Text 1\n"
-    "\n"
+    "Die IT sorgt für Umschwünge in der Finanzdienstleistungsbranche, ohne diese wäre diese Branche grundlegend anders\n"
+    "In den letzten Jahrzehnten hat sich die Art und Weise, wie Finanzdienstleistungen erbracht werden,\n"
+    "grundlegend verändert, und dies ist größtenteils auf den Einsatz von IT zurückzuführen.\n"
+    "Wir führen Ihnen Firmen vor, welche Sie als ITler mit Ihren Fähigkeiten unterstützen können.\n"    
+    "Der Bereich FinTechs(Financial Services & Technology) hat nicht die selben Geschäftsmodelle wie man\n"
+    "von Induktion erwartet.\n"
     "**Quellen:**\n"
-    "*-[Image] www.....\n"
-    "*-[1] www.....\n"
-    "*-[2] www.....\n"
+    "*-[Image] https://de.freepik.com/fotos-kostenlos/fintech-investitionskonzept-fuer-internet-finanztechnologie_17124008.htm#\n"
+    "*- Bild von rawpixel.com auf Freepik"
+    "*-[1] https://link.springer.com/chapter/10.1007/978-3-658-19131-3_14\n"
+    "*-[2] https://www.parcit.de/\n"
     )
 
-IMAGE_NAME = "image_placeholder.png"
+IMAGE_NAME = "financialservices_logo.png"
