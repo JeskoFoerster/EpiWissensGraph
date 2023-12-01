21f56e5f8a1b0d64da5af2b3db095a441b82dbca
Tom <tom.dominik.demuth@gmail.com>
2023-12-01 10:17:57 +0100

    Added A Description and a Image to MechanicalEngineering

--- IndustryNodes/MechanicalEngineering_Data.py
+++ IndustryNodes/MechanicalEngineering_Data.py
@@ -2,12 +2,13 @@ TITEL = "Maschinenbau"
 
 CONTENT = (
     "## Beschreibung:\n"
-    "Text 1\n"
+    "Maschinenbau ist ein bedeutender Ingenieurberuf und eine Schlüsselbranche, insbesondere in Deutschland. Diese Disziplin erstreckt sich über verschiedene Bereiche, von der Produktion von Kugelschreibern über Kaffeemaschinen bis hin zu Flugzeugen und modernen medizinischen Endoskopen. Der Maschinenbau umfasst traditionelle Themen wie Werkstofftechnik, Materialien und Konstruktion, während er sich gleichzeitig mit modernen Aspekten befasst, um Maschinen und Anlagen effizienter in Bezug auf Energie, Zeit und Material zu gestalten."
+    "\n\n"
+    "Die Branche zeichnet sich durch kontinuierliche Innovationen aus, die Roboter, Lasertechnik, schnelle Mikroprozessoren und optische Systeme umfassen. Die fachliche Ausrichtung des Maschinenbaustudiengangs an der HTW Berlin spiegelt die aktuellen Anforderungen der Industrie wider. Themen wie Automatisierungskonzepte, künstliche Intelligenz, Robotereinsatz, Programmierung und ressourceneffiziente Produktion stehen im Fokus des Studiums. [1]\n"
     "\n"
     "**Quellen:**\n"
-    "*-[Image] www.....\n"
-    "*-[1] www.....\n"
-    "*-[2] www.....\n"
+    "-[Image] https://cdn.prod.www.spiegel.de/images/5bb71d7c-0001-0004-0000-000001303777_w1200_r1.33_fpx54.67_fpy50.jpg\n"
+    "-https://mb-bachelor.htw-berlin.de/studium/was-ist-maschinenbau/ [1]\n"
 )
 
-IMAGE_NAME = "image_placeholder.png"
+IMAGE_NAME = "mechanicalengineering.png"
