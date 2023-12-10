a9b29b266dfef124af6cbc943197f240eda6bc60
Noah <noah_osato.asemota@smail.th-koeln.de>
2023-12-10 15:09:18 +0100

    Filled out Consulting_Data Added Image

--- IndustryNodes/Consulting_Data.py
+++ IndustryNodes/Consulting_Data.py
@@ -2,12 +2,17 @@ TITEL = "Consulting"
 
 CONTENT = (
     "## Beschreibung:\n"
-    "Text 1\n"
+    "Die IT ist im Bereich Consulting ein bedeutender Aspekt der Verarbeitung.\n"
+    "Sie hilft z.B.  bei der Datenverarbeitung mithilfe von Automatisierung und Künstlicher Intelligenz,\n"
+    "und der Entwicklung von Datenarchitekturen.\n"
+    "Wenn Sie IT in der Finanzbranche ausüben wollen und ihre Fachkenntnisse verbessern wollen,\n"
+    "dann schauen Sie sich diese Unternehmen an.\n"
     "\n"
     "**Quellen:**\n"
-    "*-[Image] www.....\n"
-    "*-[1] www.....\n"
-    "*-[2] www.....\n"
+    "*-[Image] https://de.freepik.com/fotos-kostenlos/gruppe-verschiedene-leute-die-ein-geschaeftstreffen-haben_2894621.htm#\n"
+    "*- Bild von rawpixel.com auf Freepik\n"
+    "*-[1] https://sollers.eu/de/about-us/\n"
+    "*-[2] https://sollers.eu/de/sollers-offering/\n"
     )
 
-IMAGE_NAME = "image_placeholder.png"
+IMAGE_NAME = "consulting_logo.png"
